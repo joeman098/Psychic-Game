c62fbf3c35aa102ed95594f603fbc2e48fb9401b
joeman098 <Jlogue908@gmail.com>
2017-12-07 15:21:24 -0500

    V3 even more things

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,15 +12,20 @@
   var lose = new Audio("assets/images/loseeffect.mp4");
   var wrong = new Audio("assets/images/wong.mp3");
   var win = new Audio("assets/images/win.mp3");
+  var textdifficulty = "hard";
+
+
 
    $(document).ready(function() {
-
+    document.getElementById("diff").innerHTML = textdifficulty ;
     
       $(".easyButton").on("click", function() {
         if(stats.guesesLeft == difficulty){
         stats.guesesLeft = 20;
         difficulty = 20 ;
+        textdifficulty = "easy";
         document.getElementById("guesesleft").innerHTML = stats.guesesLeft ;
+        document.getElementById("diff").innerHTML = textdifficulty ;
       }
       else{
         alert("stop cheating wait till new game to change difficulty")
@@ -32,10 +37,13 @@
         if(stats.guesesLeft == difficulty){
         stats.guesesLeft = 10;
         difficulty = 10 ;
+         textdifficulty = "medium";
         document.getElementById("guesesleft").innerHTML = stats.guesesLeft ;
+         document.getElementById("diff").innerHTML = textdifficulty ;
       }
       else{
         alert("stop cheating wait till new game to change difficulty")
+       
       }
       });
 
@@ -44,7 +52,9 @@
           if(stats.guesesLeft == difficulty){
         stats.guesesLeft = 5;
         difficulty = 5 ;
+         textdifficulty = "hard";
         document.getElementById("guesesleft").innerHTML = stats.guesesLeft ;
+        document.getElementById("diff").innerHTML = textdifficulty ;
       }
       else{
         alert("stop cheating wait till new game to change difficulty")
@@ -68,11 +78,12 @@
 
 	console.log(stats.guesesLeft);
 
-      var userPick = event.key ;
+      var userpick = event.key ;
+      var userPick = userpick.toLowerCase();
       stats.guessessofar.push(userPick);
 
-    
-      	
+    if(computerChoices.includes(userPick)){
+      	 document.getElementById("winning").style.visibility = "hidden";
       
 
  	 if (stats.guesesLeft > 1) {
@@ -82,6 +93,7 @@
           stats.computerpick = computerpick(computerpick);
           stats.guessessofar = [] ;
           win.play();
+          document.getElementById("winning").style.visibility = "visible";
           
         }
 
@@ -98,16 +110,21 @@
         stats.guessessofar = [] ;
         lose.play();
     }
-
-  
-      
-    
-
-
     document.getElementById("computerGuess").innerHTML = stats.guessessofar
     document.getElementById("wins").innerHTML = stats.wins
     document.getElementById("loses").innerHTML = stats.loses
     document.getElementById("guesesleft").innerHTML = stats.guesesLeft
+  }
+
+   else{
+    alert("Not valid input");
+  }    
+    
+
+
+    
+
+
 }
       
 
